a2da9a54da30441fb12599ead47a45a402107ecf
Anna Zholud <ankazholud@yandex.ru>
2019-09-14 19:52:09 +0300

    fragment attach interface, set data on layout

--- #7_Layouts/app/src/main/java/com/astudio/layouts/FragmentTwo.kt
+++ #7_Layouts/app/src/main/java/com/astudio/layouts/FragmentTwo.kt
@@ -1,24 +1,74 @@
 package com.astudio.layouts
 
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.astudio.layouts.entity.FlightSchedule
+import kotlinx.android.synthetic.main.fragment_one.*
+import kotlinx.android.synthetic.main.fragment_one.view.*
+
 /**
  * A simple [Fragment] subclass.
+ * The fragment layout uses any group layouts exclude ConstraintLayout.
+ * Floating action button can change day/night theme.
  *
+ * @author Anna Zholud
  */
 class FragmentTwo : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_two, container, false)
+    private var checkModeListener: OnCheckModeListener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnCheckModeListener) {
+            checkModeListener = context
+        }
+        else {
+            throw ClassCastException(context.toString() + " must implement OnCheckModeListener.")
+        }
     }
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_two, container, false)
+        arguments?.get(MainActivity.FLIGHT_AGRS_KEY)?.let { setViewData(it as FlightSchedule, view) }
+        return view
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        fab.setOnClickListener { checkModeListener?.onCheckMode() }
+    }
+
+    private fun setViewData(flightShedule: FlightSchedule, view: View) {
+        view.tv_depart_title.text = flightShedule.departFlight.flightType.toString()
+        view.tv_depart_date.text = flightShedule.departFlight.date
+        view.tv_depart_number_seats.text = flightShedule.departFlight.freeSeats
+        view.tv_depart_from.text = flightShedule.departFlight.fromCity
+        view.tv_depart_to.text = flightShedule.departFlight.toCity
+        view.tv_depart_cost.text = flightShedule.departFlight.cost
+        view.tv_depart_time_takeoff.text = flightShedule.departFlight.startTime
+        view.tv_depart_time_land.text = flightShedule.departFlight.endTime
+        view.tv_depart_flight_time.text = flightShedule.departFlight.flightTime
+
+        view.tv_return_title.text = flightShedule.returnFlight.flightType.toString()
+        view.tv_return_date.text = flightShedule.returnFlight.date
+        view.tv_return_number_seats.text = flightShedule.returnFlight.freeSeats
+        view.tv_return_from.text = flightShedule.returnFlight.fromCity
+        view.tv_return_to.text = flightShedule.returnFlight.toCity
+        view.tv_return_cost.text = flightShedule.returnFlight.cost
+        view.tv_return_time_takeoff.text = flightShedule.returnFlight.startTime
+        view.tv_return_time_land.text = flightShedule.returnFlight.endTime
+        view.tv_return_flight_time.text = flightShedule.returnFlight.flightTime
+    }
+
+    companion object {
+        fun newInstance(flightSchedule: FlightSchedule) : FragmentTwo {
+            val args = Bundle().apply { putParcelable(MainActivity.FLIGHT_AGRS_KEY, flightSchedule) }
+            val fragment = FragmentTwo().apply { arguments = args }
+            return fragment
+        }
+    }
 }
